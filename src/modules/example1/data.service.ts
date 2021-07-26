1dbefd22625f0947717c320cd946ef67e88ec898
Your Name <you@example.com>
2021-07-26 13:48:01 +0300

    modules

--- src/modules/example1/data.service.ts
+++ src/modules/example1/data.service.ts
@@ -1,23 +1,23 @@
-export const a = 1;  // export a constant
+export const a = 1;  // exports a constant
 
-export let b = 2;  // export a variable
+export let b = 2;  // exports a variable
 
-export var c = 'g';  // export a variable
+export var c = 'g';  // exports a variable
 
 const d = 3;
-export default d;   // export a default value (only one is allowed)
+export default d;   // exports a default value (only one is allowed)
 
 const e = 4;
-export { e };   // export constant with object-syntax
+export { e };   // exports a constant with object-syntax
 
 const f = 5;
 export { f };    // this one adds 'f' to the exported object, making it {e,f}
 
-export function fn() { };  // export a function
+export function fn() { };  // exports a function
 
-export const g = () => { };  // export an arrow-function
+export const g = () => { };  // exports an arrow-function
 
-export const h = { prop: 6 };  // export an object
+export const h = { prop: 6 };  // exports an object
 
-export class A { };  // export a class
+export class A { };  // exports a class
 
