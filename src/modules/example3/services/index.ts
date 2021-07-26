3b03ecdf1cb639cc34477842c682deacddd16e39
Your Name <you@example.com>
2021-07-26 15:52:06 +0300

    modules

---
+++ src/modules/example3/services/index.ts
@@ -0,0 +1,5 @@
+import { AnotherService } from './another.service';
+import { DataService } from './data.service';
+
+export const dataService = new DataService;
+export const anotherService = new AnotherService;
